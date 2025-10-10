33bb7f583acba2e896963effc7fcc79242db3411
TamNganLy <tamly1810@gmail.com>
2025-10-10 00:23:54 -0700

    fix small error

--- public/styles/style.js
+++ public/styles/style.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   const info = document.querySelector(".info");
   const img = document.querySelector(".thumbnail");
 
+   if (!info || !img) return;
+
   const adjustHeight = () => {
     const infoHeight = info.offsetHeight;
     img.style.height = infoHeight > 650 ? infoHeight + "px" : "700px";
